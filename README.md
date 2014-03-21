71b8bceb4a17af6e9f5edad7e19781c6ac0f5b30
Geo Mealer <gmealer@mozilla.com>
2014-03-20 18:07:26 -0700

    Refactored frame analysis out into MozFrameAnalyzer class

--- README.md
+++ README.md
@@ -1,35 +1,33 @@
-mozFrameAnalyzer will be a JS class you can call from any requestAnimationFrame driven animation to give you a number of statistics about the animation loop:
+MozFrameAnalyzer is a JS class you can call from any requestAnimationFrame driven animation to give you a number of statistics about the animation loop:
 
 Frame Requested: sequence of the request
 ms Elapsed since Animation Start
 ms Interval since last frame request
 
-Concurrent Frame: which frame is executing while the request is made?
-Target Frame: which frame should this request be in time for?
-Actual Frame: which frame will this request be in time for?
+Concurrent Refresh: which refresh is executing while the request is made?
+Target Refresh: which refresh should this request be in time for?
+Delivered Refresh: which refresh was this request be in time for?
 
-Concurrent Frame Deadline: at what ms does the current frame render?
-Target Frame Deadline: at what ms will or did the target frame render?
-Actual Frame Deadline: at what ms will the actual frame render?
+Concurrent Refresh Deadline: at what ms did the concurrent refresh render?
+Target Refresh Deadline: at what ms did the target refresh render?
+Delivered Frame Deadline: at what ms did the delivered refresh render?
 
-Processing Window: how many ms is there between the request and the render?
+Processing Window: how many ms was there between the request and the render?
 
-Offset Change: tells when frames start being requested ahead of their window (e.g. frame 3 requested during frame 2's render window), when the sequence catches up to synced, and when frames are skipped due to missed render windows.
+Offset Change: tells when frames start being requested ahead of their refresh (e.g. frame 3 requested during refresh 2's render window), when the sequence catches up to synced, and when refreshes are skipped due to missed render windows.
 
---- 
+---
 
-All stats are calculated by assuming the first request comes in at the beginning of a frame window (i.e. 1000/FPS ms left to render) and then tracking the timeline from there.
+All stats are calculated by assuming the first request comes in at the beginning of a refresh window (i.e. 1000/FPS ms left to render) and then tracking the timeline from there.
 
-In practice this may or may not align with reality. In particular, any early messages that the sequence is rendering a frame ahead may indicate a different offset. 
+In practice this may or may not align with reality. In particular, any early messages that the sequence is rendering a frame ahead may indicate a different offset.
 
-However, the basic timeline of one frame every 1000/FPS still applies, and while a skipped frame 5 in instrumentation might mean a skipped frame 4 or 6 in the real animation, there should still be a correlation.
+However, the basic timeline of one frame every 1000/FPS still applies, and while a skipped refresh 5 in instrumentation might mean a skipped refresh 4 or 6 in the real animation, there should still be a correlation.
 
-To recognize this possible offset discrepency, as well as to allow for the idea that a request for a new frame may be intentionally fired off shortly before the last frame renders, skipped frames are only called out when they don't offset a preceding "ahead" message. 
+To recognize this possible offset discrepency, as well as to allow for the idea that a request for a new frame may be intentionally fired off shortly before the last refresh renders, skipped refreshes are only called out when they don't offset a preceding "ahead" message.
 
-"Caught up" means that a previous frame surplus has adjusted back to sync and probably doesn't reflect a real issue. However, "Skipped," especially when the skipped amount is more than one frame, almost certainly indicates an actual stutter.
+"Caught up" means that a previous refresh surplus has adjusted back to sync and probably doesn't reflect a real issue. However, "Skipped," especially when the skipped amount is more than one refresh, almost certainly indicates an actual stutter.
 
---- 
+---
 
-The analyzer is currently all in one HTML file as a proof of concept, driving a no-op requestAnimationFrame callback. 
-
-It will be refactored out into a separate class and used to instrument various animation demos, primarily for use in verifying the accuracy of Eideticker measurements.
+The analyzer will used to instrument various animation demos, primarily for use in verifying the accuracy of Eideticker measurements.
---
+++ moz_frame_analyzer.js
@@ -0,0 +1,143 @@
+function MozFrameAnalyzer(framesExpected, refreshRate) {
+  this.reset(framesExpected);
+  this._refreshRate = refreshRate || 60;
+}
+
+MozFrameAnalyzer.prototype = {
+  get framesCaptured() {
+    return this._framesCaptured;
+  },
+
+  get framesExpected() {
+    return this._framesExpected;
+  },
+
+  get refreshRate() {
+    return this._refreshRate;
+  },
+
+  captureTimestamp: function mozFrameAnalyzer_captureTimestamp(timestamp) {
+    this._timestamps[this._framesCaptured++] = timestamp;
+  },
+
+  getFrameData: function mozFrameAnalyzer_getFrameData() {
+    const START_OFFSET = this._timestamps[0];
+    const REFRESH_INTERVAL = 1000 / this._refreshRate;
+
+    var data = [];
+    var lastElapsed = 0;
+    var targetRefresh = 1;
+
+    for (var i = 0; i < this.framesCaptured; i++) {
+      var frame = {};
+
+      frame.frameRequested = i + 1;
+      frame.elapsed = this._timestamps[i] - START_OFFSET;
+      frame.interval = (frame.elapsed - lastElapsed);
+
+      frame.concurrentRefresh = Math.floor(frame.elapsed /
+                                           REFRESH_INTERVAL) + 1;
+      frame.concurrentRefreshDeadline = frame.concurrentRefresh *
+                                        REFRESH_INTERVAL;
+
+      frame.targetRefresh = targetRefresh;
+      frame.targetRefreshDeadline = frame.targetRefresh * REFRESH_INTERVAL;
+
+      if (frame.elapsed < frame.targetRefreshDeadline) {
+        frame.deliveredRefresh = frame.targetRefresh;
+        frame.refreshesOffset = frame.deliveredRefresh -
+                                frame.concurrentRefresh;
+      } else {
+        frame.deliveredRefresh = frame.concurrentRefresh;
+        frame.refreshesOffset = frame.targetRefresh - frame.deliveredRefresh;
+      }
+      frame.deliveredRefreshDeadline = frame.deliveredRefresh *
+                                       REFRESH_INTERVAL;
+
+      frame.processingWindow = frame.deliveredRefreshDeadline - frame.elapsed;
+
+      lastElapsed = frame.elapsed;
+      targetRefresh = frame.deliveredRefresh + 1;
+
+      data.push(frame);
+    }
+
+    data.toHtmlTable = mozFrameAnalyzer_data_toHtmlTable;
+
+    return data;
+  },
+
+  reset: function mozFrameAnalyzer_reset(framesExpected) {
+    this._framesExpected = framesExpected;
+    this._framesCaptured = 0;
+    this._timestamps = new Array(framesExpected);
+
+    if (this.framesExpected) {
+      for(var i = 0; i < this.framesExpected; i++) {
+        this._timestamps[i] = -1;
+      }
+    }
+  }
+}
+
+MozFrameAnalyzer.prototype.constructor = MozFrameAnalyzer;
+
+function mozFrameAnalyzer_data_toHtmlTable() {
+  var table = ''
+  table += ('<table border="1" cellpadding="4"');
+  table += ('<tr>');
+  table += ('<th>Frame Requested</th>');
+  table += ('<th>Concurrent Refresh</th>');
+  table += ('<th>Target Refresh</th>');
+  table += ('<th>Delivered Refresh</th>');
+  table += ('<th>ms Elapsed</th>')
+  table += ('<th>ms Interval</th>');
+  table += ('<th>Concurrent Refresh Deadline</th>');
+  table += ('<th>Target Refresh Deadline</th>');
+  table += ('<th>Delivered Refresh Deadline</th>');
+  table += ('<th>Processing Window</th>');
+  table += ('<th>Offset Change</th>');
+  table += ('</tr>');
+
+  var lastOffset = 0;
+  var offsetChangeStr = '';
+
+  var len = this.length;
+  for (var i = 0; i < len; i++) {
+    if (this[i].refreshesOffset > 0 &&
+        lastOffset !== this[i].refreshesOffset) {
+      offsetChangeStr = 'Ahead +' + this[i].refreshesOffset;
+    }
+    else if (this[i].refreshesOffset === 0 && lastOffset > 0) {
+      offsetChangeStr = 'Caught up';
+    }
+    else if (this[i].refreshesOffset < 0) {
+      offsetChangeStr = '<b>Skipped ' + -(this[i].refreshesOffset) + '!</b>';
+    }
+    else {
+      offsetChangeStr = '';
+    }
+
+    table += ('<tr>');
+    table += ('<td>' + this[i].frameRequested + '</td>');
+    table += ('<td>' + this[i].concurrentRefresh + '</td>');
+    table += ('<td>' + this[i].targetRefresh + '</td>');
+    table += ('<td>' + this[i].deliveredRefresh + '</td>');
+    table += ('<td>' + this[i].elapsed.toFixed(2) + '</td>');
+    table += ('<td>' + this[i].interval.toFixed(2) + '</td>');
+    table += ('<td>' + this[i].concurrentRefreshDeadline.toFixed(2) +
+     '</td>');
+    table += ('<td>' + this[i].targetRefreshDeadline.toFixed(2) +'</td>');
+    table += ('<td>' + this[i].deliveredRefreshDeadline.toFixed(2) +
+     '</td>');
+    table += ('<td>' + this[i].processingWindow.toFixed(2) + '</td>');
+    table += ('<td>' + offsetChangeStr + '</td>');
+    table += ('</tr>');
+
+    lastOffset = this[i].refreshesOffset;
+  }
+
+  table += ('</table>');
+
+  return table;
+};
